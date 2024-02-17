87e8a128f3fe8033b59c422bdb99f99ff4189045
variskay <kerttu.a.h.varis@student.jyu.fi>
2024-02-17 18:48:58 +0200

    express exercise done

---
+++ example_project/express_exercise/Members.js
@@ -0,0 +1,23 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        email: 'jane@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Jill Doe',
+        email: 'jill@gmail.com',
+        status: 'active'
+    }
+];
+
+
+module.exports = members;
--- example_project/express_exercise/index.js
+++ example_project/express_exercise/index.js
@@ -1,10 +1,43 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+const members = require('./Members');
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send
-});
+
+// init middleware
+// app.use(logger);
+
+// Handlebars middleware
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+// Homepage route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members: members
+}));
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Members API routes
+app.use('/api/members', require('./router/api/members'));
+
+// app.get('/', (req, res) => {
+    // res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 const PORT = process.env.PORT || 5000;
 

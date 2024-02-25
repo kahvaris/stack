f27629c41f971383fb2c08e9a42ad6214ef1c6ec
variskay <kerttu.a.h.varis@student.jyu.fi>
2024-02-25 21:15:20 +0200

    Angular deletion

--- example_project/angular_exercise/src/app/components/task-item/task-item.component.ts
+++ example_project/angular_exercise/src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from '../../Task';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -11,8 +11,22 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
-export class TaskItemComponent {
+export class TaskItemComponent implements OnInit{
   @Input() task: Task = {text: '', day: '', reminder: true};
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
+  }
 
 }
